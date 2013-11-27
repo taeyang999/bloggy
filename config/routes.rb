26c0f7f6f6132c1202317bc248a1e2548250fbbb
Tae Hwang <xodid2@gmail.com>
2013-11-27 18:36:34 -0500

    changed main index page to blog index

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Bloggy::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'main#about_us'
+  root 'blogs#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
